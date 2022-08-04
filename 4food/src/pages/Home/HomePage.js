04a95ef9e54c6f2576822b6b16a9c11a1068a1d4
Talita Miguel <talita.leonardo@gmail.com>
2022-08-03 22:16:15 -0300

    montagem do filtro de categorias

--- 4food/src/pages/Home/HomePage.js
+++ 4food/src/pages/Home/HomePage.js
@@ -3,16 +3,19 @@ import { useProtectPage } from "../../hooks/useProtectPage";
 import Header from "../../component/Header/Header";
 import { requestData } from "../../services/requestAPI";
 import CardHome from "../../component/CardHome/CardHome";
-import { CardContainer, InputSearch } from "./styled";
-import InputAdornment from '@mui/material/InputAdornment';
-import Search from "../../assets/img/search.png"
-import TextField from '@mui/material/TextField';
+import { CardContainer, InputSearch, FilterCategory } from "./styled";
+import InputAdornment from "@mui/material/InputAdornment";
+import Search from "../../assets/img/search.png";
+import TextField from "@mui/material/TextField";
 import Footer from "../../component/Footer/Footer";
-import PopUpOrder  from "../../component/PopUpOrder/PopUpOrder";
+import PopUpOrder from "../../component/PopUpOrder/PopUpOrder";
+import CategorySelected from "../../component/CategorySelected/CategorySelected";
 
 export default function HomePage() {
   useProtectPage();
   const [data, setData] = useState("");
+  const [select, setSelect] = useState();
+
   useEffect(() => {
     const token = localStorage.getItem("token");
     requestData("get", "restaurants", "", token, setData);
@@ -20,6 +23,17 @@ export default function HomePage() {
 
   const getData = data?.restaurants?.map((item) => {
     return <CardHome item={item} key={item.id} />;
+  });
+
+  const getCategory = data?.restaurants?.map((item) => {
+    return (
+      <CategorySelected
+        key={item.id}
+        category={item}
+        select={select}
+        setSelect={setSelect}
+      />
+    );
   });
   return (
     <div>
@@ -33,17 +47,16 @@ export default function HomePage() {
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
-               <img src={Search} alt="lupinha"/>
+                <img src={Search} alt="lupinha" />
               </InputAdornment>
             ),
           }}
         />
       </InputSearch>
-      <CardContainer>
-        {getData}
-      </CardContainer>
+      <FilterCategory>{getCategory}</FilterCategory>
+      <CardContainer>{getData}</CardContainer>
       <PopUpOrder />
-      <Footer page={"home"}/>
+      <Footer page={"home"} />
     </div>
   );
 }
--- 4food/src/pages/Home/styled.js
+++ 4food/src/pages/Home/styled.js
@@ -1,12 +1,26 @@
 import styled from "styled-components";
 
-
 export const CardContainer = styled.div`
-margin: 0 auto;
+  margin: 0 auto;
 `;
 
 export const InputSearch = styled.div`
-width: 328px;
-margin: 0 auto;
-margin-bottom:0.5rem ;
-`
+  width: 328px;
+  margin: 0 auto;
+  margin-bottom: 0.5rem;
+`;
+
+export const FilterCategory = styled.div`
+  display: flex;
+  gap: 2.5rem;
+  font-size: 1rem;
+  overflow-x: scroll;
+  padding: 0.75rem 2.5rem;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::-webkit-scrollbar {
+    width: 6px;
+    background-color: transparent;
+  }
+
+`;
